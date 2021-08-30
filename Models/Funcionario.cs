627f21d06a5b14abe508b9b08509e9f5b493eac6
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-08-30 10:55:52 -0300

    First Application

---
+++ Models/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebTesteEntrevista.Models
+{
+
+    [Table("Funcionario")]
+    public class Funcionario
+    {
+
+        [Display(Name ="Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        public string Nome  { get; set; }
+
+        [Display(Name = "Endereço")]
+        public string Endereco  { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date)]
+        public DateTime Nascimento { get; set; }
+        public int Idade { get { return DateTime.Now.Year - Nascimento.Year; } }
+
+        [Display(Name = "Salário")]
+        public decimal Salario  { get; set; }
+    }
+}
